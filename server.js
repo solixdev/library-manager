ac352d62acf5fb0a91ef5a27765af02620d92c39
solixdev <solixdev1@gmail.com>
2024-06-30 20:42:31 +0330

    Wrote codes for fetch all users data from data base and show them as a json api

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const { createServer } = require("http");
 require("dotenv").config();
+const userController = require("./controllers/userController.js");
 
 const hostname = process.env.HOST_NAME;
 const port = process.env.PORT;
 
 const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("hello World");
+    if (req.method === "GET" && req.url === "/api/users") {
+        userController.getAllUsers(req, res);
+    }
 });
 
 server.listen(port, hostname, () => {
